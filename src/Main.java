cb42269f6ee982ad7a72b5145d2a154045e0a094
AlejandroAtondo <atondo1206@gmail.com>
2023-08-30 07:48:58 -0700

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,72 @@ public class Main {
 
         /* Expresiones Regulares
         1. Hola mundo
-        2. (?i)hola mundo
-        3. Java | Python | Go | Pascal | Perl
-        4. @(unison\.mx|uson\.mx)$
+        2. (?i).*hola mundo,*
+        3. \\b(Java|Python|Go|Pascal|Perl)\\b
+        4. ^[a-zA-Z0-9._%+-]+@(unison\.mx|uson\.mx)$
         5. ^ISI[0-9]{4}-[12](\.txt|\.csv)$
         */
 
         if (args.length != 1) {
-            System.out.println("Escribe una cadena para validar si es un nombre de archivo válido");
+            System.out.println("Escribe una cadena entre comillas para comprobar si es válida");
             return;
         }
 
         String entrada = args[0];
-        ValidarNombreArchivo(entrada);
 
+        ValidarHolaMundo(entrada);
+        ValidarHolaMundoMayus(entrada);
+        ValidarJavaPython(entrada);
+        ValidarCorreo(entrada);
+        ValidarNombreArchivo(entrada);
+    }
+
+    public static boolean ValidarHolaMundo(String cadena){
+        Pattern pat = Pattern.compile("Hola mundo");
+        Matcher mat = pat.matcher(cadena);
+        if (mat.matches()){
+            System.out.println("Hola mundo es válido.");
+        } else {
+            System.out.println("Hola mundo no es válida.");
+        }
+
+        return mat.matches();
+    }
+
+    public static boolean ValidarHolaMundoMayus(String cadena){
+        Pattern pat = Pattern.compile("(?i).*hola mundo.*");
+        Matcher mat = pat.matcher(cadena);
+        if (mat.matches()){
+            System.out.println("Hola mundo con mayusculas o minúsculas es válido.");
+        } else {
+            System.out.println("Hola mundo con mayusculas o minúsculas no es válido.");
+        }
+
+        return mat.matches();
+    }
+
+    public static boolean ValidarJavaPython(String cadena){
+        Pattern pat = Pattern.compile("\\b(Java|Python|Go|Pascal|Perl)\\b");
+        Matcher mat = pat.matcher(cadena);
+        if (mat.matches()){
+            System.out.println("Java, Python, Go, Pascal o Perl es válido.");
+        } else {
+            System.out.println("Java, Python, Go, Pascal o Perl no es válido.");
+        }
+
+        return mat.matches();
+    }
+
+    public static boolean ValidarCorreo(String cadena){
+        Pattern pat = Pattern.compile("^[a-zA-Z0-9._%+-]+@(unison\\.mx|uson\\.mx)$");
+        Matcher mat = pat.matcher(cadena);
+        if (mat.matches()){
+            System.out.println("El correo es válido.");
+        } else {
+            System.out.println("El correo no es válido.");
+        }
+
+        return mat.matches();
     }
 
     public static boolean ValidarNombreArchivo(String cadena){
